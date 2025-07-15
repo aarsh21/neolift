d4a45b0766db38a14464d25e9944d25c501a2224
aarsh21 <aarshpersonal21@gmail.com>
2025-07-15 23:44:22 +0530

    dev: setup prettier and eslint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
-  let { children } = $props();
-  import "../app.css";
+	let { children } = $props();
+	import '../app.css';
 </script>
 
-<main
-  class="flex flex-col h-screen w-full bg-black items-center justify-center"
->
-  {@render children()}
+<main class="flex flex-col h-screen w-full bg-black items-center justify-center">
+	{@render children()}
 </main>
